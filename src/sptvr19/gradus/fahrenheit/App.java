75a4573a58526e3b6bc6a52f99f9a3adf779001a
Bakugan <kafder97@gmail.com>
2020-09-11 11:22:19 +0300

    Добавлен конвентатор Кельвина в цельсия и наоборот

--- src/sptvr19/gradus/fahrenheit/App.java
+++ src/sptvr19/gradus/fahrenheit/App.java
@@ -17,23 +17,38 @@ class App {
         double f;
         double c;
         int d = 32;
+        double k = 273.15;
         double b = 1.8;
+        double z;
         System.out.println("Конвентер градусов Цельсия в градусы по Фаренгейту или наоборот.");
         scanner.nextLine();
-        System.out.println("Выберите: (1)-Цельсия в фаренгейт (2) - наоборот: ");
+        System.out.println("Выберите: (1)-Цельсия в фаренгейт (2) - Цельсия в фаренгейт (3) - Цельсия в кельвин (4) - Кельвин в Цельсия: ");
         String znak = scanner.nextLine();
         if(null != znak) switch (znak) {
             case "1":
                 System.out.println("Введите градусы по Цельсию: ");
-                double gradC = scanner.nextDouble();
-                c = gradC * b + d;
+                double gradC1 = scanner.nextDouble();
+                c = gradC1 * b + d;
                 System.out.println("По фаренгейту: " + c);
                 break;
             case "2":
                 System.out.println("Введите градусы по Фаренгейту: ");
-                double gradF = scanner.nextDouble();
-                f = (gradF - d) / b;
+                double gradF1 = scanner.nextDouble();
+                f = (gradF1 - d) / b;
                 System.out.println("По цельсию: " + f);
+            case "3":
+                System.out.println("Введите градусы по Цельсию: ");
+                double gradC2 = scanner.nextDouble();
+                z = gradC2 + k;
+                System.out.println("По кельвина:" + z);
+            case "4":
+                System.out.println("Введите градусы по Кельвина: ");
+                double gradK1 = scanner.nextDouble();
+                z = gradK1 - k;
+                System.out.println("По цельсия:" + z);
+
+                
+                
         }
     }
 }
